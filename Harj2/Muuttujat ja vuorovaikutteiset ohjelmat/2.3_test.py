f45770524cac1d8812afcfe5b30ad96fc4b5ca10
mirodahl <miro.dahlgren@metropolia.fi>
2023-01-12 13:10:25 +0200

    Empty base Harj1 palautus Harj2 alustus 2.1 valmis 2.2 alustus 2.2 aloittelu, vielä viallinen 2 osio alustus 2.3-2.6

---
+++ Harj2/Muuttujat ja vuorovaikutteiset ohjelmat/2.3_test.py
@@ -0,0 +1,4 @@
+##Kirjoita ohjelma, joka kysyy suorakulmion kannan ja korkeuden.
+##Ohjelma tulostaa suorakulmion piirin ja pinta-alan.
+##Suorakulmion piiri tarkoittaa sen neljän sivun yhteispituutta.
+##
---
+++ Harj2/Muuttujat ja vuorovaikutteiset ohjelmat/2.4_test.py
@@ -0,0 +1,2 @@
+##Kirjoita ohjelma, joka kysyy kolme kokonaislukua
+##Ohjelma tulostaa lukujen summan, tulon ja keskiarvon.
