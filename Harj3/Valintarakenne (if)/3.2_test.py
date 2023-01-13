a6b72acadffe50537f5b15f48a25ad085a3778d2
mirodahl <miro.dahlgren@metropolia.fi>
2023-01-13 08:48:01 +0200

    Valmiit tähän mennessä [13.01.23]: 1,2.1,2.2,2.3,2.4,2.5,2.6,3,3 To do 3.2,3.3,3.4

--- Harj3/Valintarakenne (if)/3.2_test.py
+++ Harj3/Valintarakenne (if)/3.2_test.py
@@ -8,3 +8,18 @@
 ##C on ikkunaton hytti autokannen alapuolella.
 ##Jos käyttäjä syöttää kelvottoman hyttiluokan, ohjelma tulostaa Virheellinen hyttiluokka.
 
+print("Tervetuloa laivalle!")
+print("Meillä on seuraavat hyttiluokat tarjolla: LUX, A, B ja C")
+print("LUX on parvekkeellinen hytti yläkannella")
+print("A on ikkunallinen hytti autokannen yläpuolella")
+print("B on ikkunaton hytti autokannen yläpuolella")
+print("C on ikkunaton hytti autokannen alapuolella")
+
+cabin_class = input("Ole hyvä ja valitse mieleisesi hyttiluokka: ")
+
+print("Kiitos valinnastasi!") if input == "LUX" or "A" or "B" or "C" else print("Virheellinen hyttiluokka!")
+
+#if input == "LUX" or "A" or "B" or "C":
+#    print("Kiitos valinnastasi!")
+#    else:
+#    print("Virheellinen hyttiluokka!")
