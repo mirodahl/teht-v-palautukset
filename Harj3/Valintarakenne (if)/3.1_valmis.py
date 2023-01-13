37ed017c8427bdf3f000476705ffa7ca9ad4389c
mirodahl <miro.dahlgren@metropolia.fi>
2023-01-13 08:21:45 +0200

    Valmiit tähän mennessä [13.01.23]: 1,2.1,2.2,2.3,2.4,2.5,2.6,3,3 To do 3.2,3.3,3.4

--- Harj3/Valintarakenne (if)/3.1_valmis.py
+++ Harj3/Valintarakenne (if)/3.1_valmis.py
@@ -3,3 +3,12 @@
 ## montako senttiä alimmasta sallitusta pyyntimitasta puuttuu.
 ##Kuha on alamittainen, jos sen pituus on alle 37 cm.
 
+print("Tarkistetaan ettei kuhasi ole alamittainen!")
+fish = float(input("Syötä saamasi kuhan pituus senttimetreinä: "))
+
+if fish>=37:
+    print("Kuhasi on tarpeeksi suuri.")
+
+if fish<=37:
+    undersize = 37 - fish
+    print(f"Kuhasi on {undersize}cm alamittainen, laske se takaisin järveen!")
