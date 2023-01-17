1961d66f6c99c8d1ef4a505acd9c85cb3b6383a7
mirodahl <miro.dahlgren@metropolia.fi>
2023-01-17 15:19:24 +0200

    Valmiit tähän mennessä [17.01.23]: 1, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 3.1, 3.4, To do 3.2,3.3

--- Harj3/Valintarakenne (if)/3.3_test.py
+++ Harj3/Valintarakenne (if)/3.3_test.py
@@ -1,15 +1,23 @@
-##Kirjoita ohjelma, joka kysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l).
-##Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
+#Kirjoita ohjelma, jokakysyy käyttäjän biologisen sukupuolen ja hemoglobiiniarvon (g/l).
+#Ohjelma ilmoittaa, onko hemoglobiiniarvo alhainen, normaali vai korkea.
 
-##Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
-##Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
+#Naisen normaali hemoglobiiniarvo on välillä 117-175 g/l.
+#Miehen normaali hemoglobiiniarvo on välillä 134-195 g/l.
 
 gender = int(input("Syötä sukupuoli, 1 = mies, 2 = nainen: "))
 hg = int(input("Syötä hemoglobiiniarvo g/l: "))
 
-if (gender == 1) and (134 >= hg <= 195):
+if gender == 1 and (134 > hg < 195):
     print("Hemoglobiini on normaali")
-elif (gender == 1) and (hg < 134):
+elif gender == 1 and (hg < 134):
     print("Hemoglobiini on alhainen")
-elif (gender == 1) and (hg > 195):
+elif gender == 1 and (hg > 195):
+    print("Hemogblobiini on korkea")
+
+elif gender == 2 and (117 > hg < 175):
+    print("Hemoglobiini on normaali")
+elif gender == 2 and (hg < 117):
+    print("Hemoglobiini on alhainen")
+else:
+    gender == 2 and (hg > 175)
     print("Hemogblobiini on korkea")
