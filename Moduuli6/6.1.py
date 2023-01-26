937682a54300d9fba0af2c680bc375e1af0c9c91
mirodahl <miro.dahlgren@metropolia.fi>
2023-01-26 14:50:29 +0200

    Valmiit tähän mennessä [26.01.23]: 5.1 To do: 4.6,5.3,6.1-6.6

--- Moduuli6/6.1.py
+++ Moduuli6/6.1.py
@@ -1,3 +1,19 @@
-#Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun väliltä 1..6.
+#Kirjoita parametriton funktio, joka palauttaa paluuarvonaan satunnaisen nopan silmäluvun
+# väliltä 1..6.
 # Kirjoita pääohjelma, joka heittää noppaa niin kauan kunnes tulee kuutonen.
 # Pääohjelma tulostaa kunkin heiton jälkeen saadun silmäluvun.
+import random
+diceresult = 0
+print("Heitän noppaa niin kauan kunnes saan silmäluvun 6")
+
+
+def noppa():
+    random.randint(1, 6)
+
+while diceresult < 1:
+    if noppa < 6:
+        print(noppa())
+        diceresult = 0
+    else:
+        print(noppa())
+        diceresult = diceresult + 1
--- Moduuli6/6.6.py
+++ Moduuli6/6.6.py
@@ -3,3 +3,8 @@
 # Pääohjelma kysyy käyttäjältä kahden pizzan halkaisijat ja hinnat sekä ilmoittaa,
 # kumpi pizza antaa paremman vastineen rahalle (eli kummalla on alhaisempi yksikköhinta).
 # Yksikköhintojen laskennassa on hyödynnettävä kirjoitettua funktiota.
+
+pizzawidth1 = int(input("Ensimmäisen pitsan halkaisija: "))
+pizzaprice1 = int(input("Ensimmäisen pitsan hinta: "))
+pizzawidth2 = int(input("Toisen pitsan halkaisija: "))
+pizzaprice2 = int(input("Toisen pitsan hinta: "))
