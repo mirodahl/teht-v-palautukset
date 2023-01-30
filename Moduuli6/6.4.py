06cd159637c2eeab28f71f490f9b4a3aa16aff55
mirodahl <miro.dahlgren@metropolia.fi>
2023-01-30 15:20:24 +0200

    Valmiit tähän mennessä [30.01.23]: 6.1,6.2,6.3 To do: 4.6,5.3,6.4-6.6,7.1-7.3

--- Moduuli6/6.4.py
+++ Moduuli6/6.4.py
@@ -1,3 +1,18 @@
 #Kirjoita funktio, joka saa parametrinaan listan kokonaislukuja.
 # Ohjelma palauttaa listassa olevien lukujen summan.
 # Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen palauttaman summan.
+numbers = []
+def abc():
+    print(sum(numbers))
+
+
+number = input("Anna kokonaisluku: ")
+numbers.append(number)
+
+while number != "":
+    number = input("Anna seuraava kokonaisluku: ")
+    numbers.append(number)
+
+print(abc())
+
+
--- Moduuli6/6.5.py
+++ Moduuli6/6.5.py
@@ -3,3 +3,16 @@
 # paitsi että siitä on karsittu pois kaikki parittomat luvut.
 # Kirjoita testausta varten pääohjelma, jossa luot listan, kutsut funktiota ja tulostat sen jälkeen
 # sekä alkuperäisen että karsitun listan.
+numbers = []
+def abc():
+    print(numbers)
+
+
+number = input("Anna kokonaisluku: ")
+numbers.append(number)
+
+while number != "":
+    number = input("Anna seuraava kokonaisluku: ")
+    numbers.append(number)
+
+print(abc())
