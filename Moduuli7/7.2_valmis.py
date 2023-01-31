563a19e2bec0c76563c708b4f8ca1589143a5987
mirodahl <miro.dahlgren@metropolia.fi>
2023-01-31 06:38:14 +0200

    Valmiit tähän mennessä [31.01.23]: 6.1,6.2,6.3,7.1,7.2 To do: 4.6,5.3,6.4-6.6,7.3

---
+++ Moduuli7/7.2_valmis.py
@@ -0,0 +1,21 @@
+#Kirjoita ohjelma, joka kysyy käyttäjältä nimiä siihen saakka, kunnes käyttäjä syöttää tyhjän merkkijonon.
+# Kunkin nimen syöttämisen jälkeen ohjelma tulostaa joko tekstin Uusi nimi tai Aiemmin syötetty nimi sen mukaan,
+# syötettiinkö nimi ensimmäistä kertaa.
+# Lopuksi ohjelma luettelee syötetyt nimet yksi kerrallaan allekkain mielivaltaisessa järjestyksessä.
+# Käytä joukkotietorakennetta nimien tallentamiseen.
+#import random
+names = set()
+
+print("Kyselen sinulta nimiä. Voit lopettaa ohjelman suorittamisen, syöttämällä tyhjän.")
+name = input("Anna nimi: ")
+
+while name != "":
+    if name in names:
+        print("Nimi syötetty aikaisemmin")
+    else:
+        print("Uusi nimi tallennettu")
+        names.add(name)
+    name = input("Anna uusi nimi: ")
+
+#random.shuffle(names)
+print(names)
