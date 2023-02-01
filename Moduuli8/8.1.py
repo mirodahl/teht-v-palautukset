ed88b6571069d7c922864c36c38ebde1d70c1763
mirodahl <miro.dahlgren@metropolia.fi>
2023-02-01 10:27:26 +0200

    Valmiit tähän mennessä [01.02.23]: 6.1,6.2,6.3,7.1,7.2,6.6 To do: 4.6,5.3,6.4-6.5,7.3,8.1-8.3

--- Moduuli8/8.1.py
+++ Moduuli8/8.1.py
@@ -1,5 +1,17 @@
 import mysql.connector
 
+def searchAirports(ICAO):
+    sql = "SELECT Numero, Sukunimi, Etunimi, Palkka FROM airports"
+    sql += " WHERE ICAO='" + ICAO + "'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0 :
+        for rivi in tulos:
+            print(f"Päivää! Olen {rivi[2]} {rivi[1]}. Palkkani on {rivi[3]} euroa kuussa.")
+    return
+
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
@@ -8,3 +20,6 @@ connection = mysql.connector.connect(
          password='sAL_a3ana',
          autocommit=True
          )
+
+ICAO = input("Anna ICAO-koodi: ")
+searchAirports(ICAO)
--- Moduuli8/8.3.py
+++ Moduuli8/8.3.py
@@ -1,5 +1,17 @@
 import mysql.connector
 
+def haeTyöntekijätSukunimellä(sukunimi):
+    sql = "SELECT Numero, Sukunimi, Etunimi, Palkka FROM Työntekijä"
+    sql += " WHERE Sukunimi='" + sukunimi + "'"
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount >0 :
+        for rivi in tulos:
+            print(f"Päivää! Olen {rivi[2]} {rivi[1]}. Palkkani on {rivi[3]} euroa kuussa.")
+    return
+
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
@@ -8,3 +20,6 @@ connection = mysql.connector.connect(
          password='sAL_a3ana',
          autocommit=True
          )
+
+sukunimi = input("Anna sukunimi: ")
+haeTyöntekijätSukunimellä(sukunimi)
