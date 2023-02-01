e19410fc35cd20909f336b96e972aaaca872de3f
mirodahl <miro.dahlgren@metropolia.fi>
2023-02-01 10:21:10 +0200

    Valmiit tähän mennessä [01.02.23]: 6.1,6.2,6.3,7.1,7.2,6.6 To do: 4.6,5.3,6.4-6.5,7.3,8.1-8.3

--- Moduuli8/8.1.py
+++ Moduuli8/8.1.py
@@ -1,1 +1,10 @@
 import mysql.connector
+
+connection = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='airports',
+         user='dbuser',
+         password='sAL_a3ana',
+         autocommit=True
+         )
