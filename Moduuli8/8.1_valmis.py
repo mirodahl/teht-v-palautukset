16d9c8ba1d068cedb3d2c311d5ec76b8ed73cd12
mirodahl <miro.dahlgren@metropolia.fi>
2023-02-02 15:13:20 +0200

    Valmiit tähän mennessä [02.02.23]: 8.1 To do: 4.6,5.3,6.4-6.5,7.3,8.2-8.3

--- Moduuli8/8.1_valmis.py
+++ Moduuli8/8.1_valmis.py
@@ -2,27 +2,27 @@ import mysql.connector
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
-         port= 3306,
+         port=3306,
          database='flight_game',
          user='user1',
          password='sala1',
          autocommit=True
          )
 
-def searchAirports(ICAO):
-    sql = "SELECT name, municipality" +\
-          "FROM airport"
-    sql += " WHERE ident='" + ICAO + "'"
+def search_airports(ICAO):
+    sql = "SELECT name, municipality " +\
+          "FROM airport " +\
+          " WHERE ident='" + ICAO + "'"
     print(sql)
     kursori = connection.cursor()
     kursori.execute(sql)
     tulos = kursori.fetchall()
-    if kursori.rowcount >0 :
+    if kursori.rowcount > 0:
         for rivi in tulos:
             print(f"ICAO-koodilla löytynyt kenttä on {rivi[0]} ja sen sijainti on {rivi[1]}")
     return
 
 ICAO = input("Anna ICAO-koodi: ")
-print(searchAirports(ICAO))
+print(search_airports(ICAO))
 
 connection.close()
--- Moduuli8/8.3.py
+++ Moduuli8/8.3.py
@@ -2,7 +2,7 @@ import mysql.connector
 
 connection = mysql.connector.connect(
          host='127.0.0.1',
-         port= 3306,
+         port=3306,
          database='flight_game',
          user='user1',
          password='sala1',
