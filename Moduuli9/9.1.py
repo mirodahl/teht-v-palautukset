b78a17561e72d3ecfc3da23e068a653074cd0f6f
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-15 13:00:39 +0200

    Valmiit tähän mennessä [14.03.23]: To do: 9.1-4

---
+++ Moduuli9/9.1.py
@@ -0,0 +1,6 @@
+#Kirjoita Auto-luokka, jonka ominaisuuksina ovat
+# rekisteritunnus, huippunopeus, tämänhetkinen nopeus ja kuljettu matka.
+# Kirjoita luokkaan alustaja, joka asettaa ominaisuuksista kaksi ensin mainittua parametreina saatuihin arvoihin.
+# Uuden auton nopeus ja kuljetut matka on asetettava automaattisesti nollaksi.
+# Kirjoita pääohjelma, jossa luot uuden auton (rekisteritunnus ABC-123, huippunopeus 142 km/h).
+# Tulosta pääohjelmassa sen jälkeen luodun auton kaikki ominaisuudet.
