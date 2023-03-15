1b95e643005c1aa725ca49bb588ff888a7b5d62b
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-15 14:34:26 +0200

    Valmiit tähän mennessä [15.03.23]: 9.1-2 To do: 9.3-4

--- Moduuli9/9.2.py
+++ Moduuli9/9.2.py
@@ -1,4 +1,5 @@
-#Jatka ohjelmaa kirjoittamalla Auto-luokkaan kiihdytä-metodi, joka saa parametrinaan nopeuden muutoksen
+#Jatka ohjelmaa kirjoittamalla Auto-luokkaan kiihdytä-metodi,
+# joka saa parametrinaan nopeuden muutoksen
 # (km/h). Jos nopeuden muutos on negatiivinen, auto hidastaa.
 # Metodin on muutettava auto-olion nopeus-ominaisuuden arvoa.
 # Auton nopeus ei saa kasvaa huippunopeutta suuremmaksi eikä alentua nollaa pienemmäksi.
@@ -6,3 +7,28 @@
 # Tulosta tämän jälkeen auton nopeus.
 # Tee sitten hätäjarrutus määräämällä nopeuden muutos -200 km/h ja tulosta uusi nopeus.
 # Kuljettua matkaa ei tarvitse vielä päivittää.
+
+class Car:
+    def __init__(self, regplate, topspd):
+        self.regplate = regplate
+        self.topspd = topspd
+        self.curspd = 0
+        self.travdist = 0
+    def accelerate(self, change):
+        self.curspd = self.curspd + change
+        if self.curspd > self.topspd:
+            self.curspd = self.topspd
+        if self.curspd < 0:
+            self.curspd = 0
+        return print(f"Tämän hetkinen nopeus {car.curspd} km/h")
+
+
+car = Car("ABC-123", 142)
+print(f"Rekisteritunnuksella {car.regplate} olevan auton huippunopeus {car.topspd} km/h, \n"
+      f"tämän hetkinen nopeus {car.curspd} km/h ja kuljettu matka {car.travdist} km.")
+car.accelerate(30)
+car.accelerate(70)
+car.accelerate(50)
+car.accelerate(-200)
+
+
