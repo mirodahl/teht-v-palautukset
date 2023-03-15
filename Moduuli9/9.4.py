4d4b2f8af6851abfee659df060e6363d1ef3c38b
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-15 15:17:50 +0200

    Valmiit tähän mennessä [15.03.23]: 9.1-3 To do: 9.4, 10.1-4

--- Moduuli9/9.4.py
+++ Moduuli9/9.4.py
@@ -9,3 +9,34 @@
 #Kaikkia autoja käsketään liikkumaan yhden tunnin ajan. Tämä tehdään kutsumalla kulje-metodia.
 #Kilpailu jatkuu, kunnes jokin autoista on edennyt vähintään 10000 kilometriä.
 # Lopuksi tulostetaan kunkin auton kaikki ominaisuudet selkeäksi taulukoksi muotoiltuna.
+import random
+class Car:
+
+    car_count = 0
+    def __init__(self, regplate, topspd):
+        Car.car_count += 1
+        print(f"{Car.car_count}. Auto luotu, rekno: {regplate}, huippunopeus {topspd}")
+        self.regplate = regplate
+        self.topspd = topspd
+        self.curspd = 0
+        self.travdist = 0
+    def accelerate(self, change):
+        self.curspd = self.curspd + change
+        if self.curspd > self.topspd:
+            self.curspd = self.topspd
+        if self.curspd < 0:
+            self.curspd = 0
+        return
+    def travel(self, hours):
+        change = self.curspd * hours
+        self.travdist = self.travdist + change
+        return
+
+
+cars = []
+for i in range(10):
+    cars.append(Car("ABC- " + str(i+1), random.randint(100, 200)))
+
+while cars.travdist < 1000:
+    for i in range(10):
+        cars.accelerate(random.randint -10, 15)
