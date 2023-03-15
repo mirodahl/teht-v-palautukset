9d7ae62a6e85b881e0a473c95e981fe85e833348
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-15 13:22:37 +0200

    Valmiit tähän mennessä [15.03.23]: 9.1 To do: 9.2-4

--- Moduuli9/9.1.py
+++ Moduuli9/9.1.py
@@ -4,3 +4,15 @@
 # Uuden auton nopeus ja kuljetut matka on asetettava automaattisesti nollaksi.
 # Kirjoita pääohjelma, jossa luot uuden auton (rekisteritunnus ABC-123, huippunopeus 142 km/h).
 # Tulosta pääohjelmassa sen jälkeen luodun auton kaikki ominaisuudet.
+
+class Car:
+    def __init__(self, regplate, topspd):
+        self.regplate = regplate
+        self.topspd = topspd
+        self.curspd = 0
+        self.travdist = 0
+
+
+car = Car("ABC-123", 142)
+print(f"Rekisteritunnuksella {car.regplate} olevan auton huippunopeus {car.topspd} km/h, \n"
+      f"tämän hetkinen nopeus {car.curspd} ja kuljettu matka {car.travdist} km.")
