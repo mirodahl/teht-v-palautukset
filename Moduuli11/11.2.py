8e70295bfbfc67d431d12730313b95c2b802e485
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-17 16:48:43 +0200

    Valmiit tähän mennessä [17.03.23]: 9.1-4,10.1,11.2 To do: 10.2-4,11.1,12.1-2,13.1-2

--- Moduuli11/11.2.py
+++ Moduuli11/11.2.py
@@ -8,6 +8,7 @@
 # yhden polttomoottoriauton (ACD-123, 165 km/h, 32.3 l).
 # Aseta kummallekin autolle haluamasi nopeus, käske autoja ajamaan kolmen tunnin verran ja
 # tulosta autojen matkamittarilukemat.
+import random
 cars = []
 class Car:
     def __init__(self, reg_plate, top_spd):
@@ -15,6 +16,7 @@ class Car:
         self.top_spd = top_spd
         self.cur_spd = 0
         self.trav_dist = 0
+        print(f"Auto luotu, rekno: {reg_plate}, huippunopeus {top_spd}")
 
     def accelerate(self, change):
         self.cur_spd = self.cur_spd + change
@@ -22,11 +24,12 @@ class Car:
             self.cur_spd = self.top_spd
         if self.cur_spd < 0:
             self.cur_spd = 0
-        return print(f"Tämän hetkinen nopeus {car.cur_spd} km/h")
+        return print(f"Auton {self.reg_plate} tämän hetkinen nopeus {self.cur_spd} km/h")
 
     def travel(self, hours):
         change = self.cur_spd * hours
         self.trav_dist = self.trav_dist + change
+        print(f"Autolla {self.reg_plate} ajetaan {hours} tuntia")
         return self.trav_dist
 
 class ElectricCar(Car):
@@ -44,3 +47,12 @@ class PetrolCar(Car):
 
 cars.append(ElectricCar("ABC-15", 180, 52.5))
 cars.append(PetrolCar("ABD-123", 165, 32.3))
+
+for car in cars:
+    car.accelerate(200)
+    car.travel(3)
+
+print("Nyt ollaan ajettu haluttu aika, katsotaas kuinka monta kilometriä kertyikään mittariin")
+
+for car in cars:
+    print(f"Autolla {car.reg_plate} on ajettu {car.trav_dist} kilometriä")
