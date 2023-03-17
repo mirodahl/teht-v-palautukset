3fe8794ce87f335892f2e6a6514484e1633c131a
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-17 17:40:39 +0200

    Valmiit tähän mennessä [17.03.23]: 9.1-4,10.1,11.1-2 To do: 10.2-4,12.1-2,13.1-2

--- Moduuli11/11.1.py
+++ Moduuli11/11.1.py
@@ -7,12 +7,41 @@
 # Luo pääohjelmassa julkaisut Aku Ankka (päätoimittaja Aki Hyyppä)
 # ja Hytti n:o 6 (kirjailija Rosa Liksom, 200 sivua).
 # Tulosta molempien julkaisujen kaikki tiedot toteuttamiesi metodien avulla.
-
+books = []
+magazines = []
 class Publish:
-    pass
+    def __init__(self, name, author):
+        self.name = name
+        self.author = author
+        print(f"Teos {name} luotu.")
 
 class Magazine(Publish):
-    pass
+    magazine_count = 0
+    def __init__(self, name, author):
+        Magazine.magazine_count += 1
+        Publish.__init__(self, name, author)
+
+    def mag_details(self):
+        for magazine in magazines:
+            return print(f"Lehden nimi: {self.name}, Päätoimittaja {self.author}")
+
 
 class Book(Publish):
-    pass
+    book_count = 0
+    def __init__(self, name, author, pages):
+        Book.book_count += 1
+        Publish.__init__(self, name, author)
+        self.pages = pages
+
+    def book_details(self):
+        for book in books:
+            return print(f"Kirjan nimi: {self.name}, Kirjailija {self.author}, Sivujen lkm; {self.pages}")
+
+books.append(Book("Hytti n:o 6", "Rosa Liksom", 200))
+magazines.append(Magazine("Aku Ankka", "Aki Hyyppä"))
+
+for book in books:
+    book.book_details()
+
+for magazine in magazines:
+    magazine.mag_details()
