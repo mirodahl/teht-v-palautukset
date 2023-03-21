a69102af848617bef89cbe02d8e8cbc430d6effa
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-21 11:49:35 +0200

    Valmiit tähän mennessä [21.03.23]: 9.1-4,10.1,11.1-2,12.1 To do: 10.2-4,12.2,13.1-2

--- Moduuli12/12.2.py
+++ Moduuli12/12.2.py
@@ -1,5 +1,22 @@
 #Tutustu avoimeen OpenWeather-säärajapintaan: https://openweathermap.org/api.
-# Kirjoita ohjelma, joka kysyy käyttäjältä paikkakunnan nimen ja tulostaa sitä vastaavan säätilan tekstin
+# Kirjoita ohjelma, joka kysyy käyttäjältä paikkakunnan nimen ja tulostaa
+# sitä vastaavan säätilan tekstin
 # sekä lämpötilan Celsius-asteina. Perehdy rajapinnan dokumentaatioon riittävästi.
 # Palveluun rekisteröityminen on tarpeen, jotta saat rajapintapyynnöissä tarvittavan API-avaimen (API key).
 # Selvitä myös, miten saat Kelvin-asteet muunnettua Celsius-asteiksi.
+
+#api call: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+#api a2061d23796da1bbbaa2a3443766ed9d
+
+import requests
+import json
+
+search = input("Anna haettava paikkakunta: ")
+
+try:
+        request = "https://api.openweathermap.org/data/2.5/weather?q="+search+"&units=metric&appid=5b78d00bef03aee5575482775fdca0e9"
+        answer = requests.get(request).json()
+        print(answer)
+        #print(json.dumps(answer['weather.description']["main.temp"]))
+except requests.exceptions.RequestException as e:
+    print ("Hakua ei voitu suorittaa.")
