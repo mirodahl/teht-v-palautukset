80e3dad8d446ac99d1b539a0bb7b24ec5f4d91d6
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-21 11:59:44 +0200

    Valmiit tähän mennessä [21.03.23]: 9.1-4,10.1,11.1-2,12.1 To do: 10.2-4,12.2,13.1-2

--- Moduuli13/13.1.py
+++ Moduuli13/13.1.py
@@ -1,1 +1,15 @@
-#Toteuta Flask-taustapalvelu, joka ilmoittaa, onko parametrina saatu luku alkuluku vai ei. Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin. Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31. Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
+#Toteuta Flask-taustapalvelu, joka ilmoittaa, onko parametrina saatu luku alkuluku vai ei.
+# Hyödynnä toteutuksessa aiempaa tehtävää, jossa alkuluvun testaus tehtiin.
+# Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31.
+# Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
+
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/prime')
+def prime():
+    param = request.param
+    Prime = False
+    pass
+    Prime = True
+    return Prime
