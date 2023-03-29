eb741debc157f8609060f420b35c795710a3de0e
mirodahl <miro.dahlgren@metropolia.fi>
2023-03-29 13:23:53 +0300

    Valmiit tähän mennessä [29.03.23]: 12.1-2 To do: 10.2-4,13.2

--- Moduuli13/13.1.py
+++ Moduuli13/13.1.py
@@ -3,13 +3,25 @@
 # Esimerkiksi lukua 31 vastaava GET-pyyntö annetaan muodossa: http://127.0.0.1:3000/alkuluku/31.
 # Vastauksen on oltava muodossa: {"Number":31, "isPrime":true}.
 
-from flask import Flask, request
+from flask import Flask, request, Response, jsonify
 
 app = Flask(__name__)
-@app.route('/prime')
-def prime():
-    param = request.param
-    Prime = False
-    pass
+
+def primer(num):
     Prime = True
+    for i in range(2, num):
+        if num % i == 0:
+        Prime = False
     return Prime
+
+@app.route("/prime/<int:n>")
+def primed(num):
+    result = primer(num)
+    answer = {
+        "number": num,
+        "primed": result
+    }
+    return jsonify(answer)
+
+if __name__ == "__main__":
+    app.run(debug = True, port = 3000)
